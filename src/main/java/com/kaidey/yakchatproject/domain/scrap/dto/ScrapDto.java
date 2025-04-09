bbc2dbb67d2eabbf33ea8adea919463740345279
parkjanghoon <pparkcoder@gmail.com>
2025-04-10 00:19:25 +0900

    나의 질문,답변보기 / 스크랩 기능 개발

---
+++ src/main/java/com/kaidey/yakchatproject/domain/scrap/dto/ScrapDto.java
@@ -0,0 +1,14 @@
+package com.kaidey.yakchatproject.domain.scrap.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ScrapDto {
+    private Long id;
+    private Long scraperId;
+    private Long questionId;
+}
---
+++ src/main/java/com/kaidey/yakchatproject/domain/scrap/entity/Scrap.java
@@ -0,0 +1,31 @@
+package com.kaidey.yakchatproject.domain.scrap.entity;
+
+import com.kaidey.yakchatproject.domain.answer.entity.Answer;
+import com.kaidey.yakchatproject.domain.question.entity.Question;
+import com.kaidey.yakchatproject.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Scrap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long scraperId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question; // 스크랩 질문
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
