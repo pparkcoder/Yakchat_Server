59b425507c1fd2bdca298376105d2d0629246e2e
parkjanghoon <pparkcoder@gmail.com>
2025-04-26 15:29:46 +0900

    아카이브 질문보기/답변보기 소스 수정

--- src/main/java/com/kaidey/yakchatproject/domain/archive/controller/ArchiveController.java
+++ src/main/java/com/kaidey/yakchatproject/domain/archive/controller/ArchiveController.java
@@ -2,20 +2,20 @@ package com.kaidey.yakchatproject.domain.archive.controller;
 
 import com.kaidey.yakchatproject.domain.answer.dto.AnswerDto;
 import com.kaidey.yakchatproject.domain.answer.entity.Answer;
+import com.kaidey.yakchatproject.domain.answer.service.AnswerService;
 import com.kaidey.yakchatproject.domain.archive.service.ArchiveService;
 import com.kaidey.yakchatproject.domain.image.util.ImageUtils;
 import com.kaidey.yakchatproject.domain.question.dto.QuestionDto;
+import com.kaidey.yakchatproject.domain.question.dto.QuestionWithAnswersDto;
 import com.kaidey.yakchatproject.domain.question.entity.Question;
+import com.kaidey.yakchatproject.domain.question.service.QuestionService;
 import com.kaidey.yakchatproject.domain.scrap.dto.ScrapDto;
-import com.kaidey.yakchatproject.domain.user.entity.User;
 import com.kaidey.yakchatproject.domain.user.service.UserService;
 import com.kaidey.yakchatproject.global.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/archive")
@@ -24,34 +24,33 @@ public class ArchiveController {
     private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
     private final ArchiveService archiveService;
+    private final QuestionService questionService;
+    private final AnswerService answerService;
     private final ImageUtils imageUtils = new ImageUtils();
 
     @Autowired
-    public ArchiveController(UserService userService, JwtTokenProvider jwtTokenProvider, ArchiveService archiveService) {
+    public ArchiveController(UserService userService, JwtTokenProvider jwtTokenProvider, ArchiveService archiveService
+                , QuestionService questionService, AnswerService answerService) {
         this.userService = userService;
         this.jwtTokenProvider = jwtTokenProvider;
         this.archiveService = archiveService;
+        this.questionService = questionService;
+        this.answerService = answerService;
     }
 
-    // 자신이 적은 모든 질문 조회
+    // 내 질문 조회(최신순 5개)
     @GetMapping("/my-questions")
-    public ResponseEntity<List<QuestionDto>> getMyAllQuestions(@RequestHeader("Authorization") String token){
+    public ResponseEntity<List<QuestionWithAnswersDto>> getMyAllQuestions(@RequestHeader("Authorization") String token){
         Long userId = jwtTokenProvider.getUserIdFromToken(token.substring(7));
-        User findUser = userService.getUserById(userId);
-        List<QuestionDto> questions = findUser.getQuestions().stream()
-                .map(this::convertToQuestionDto)
-                .collect(Collectors.toList());
+        List<QuestionWithAnswersDto> questions = questionService.getQuestionsByUserIdByCreatedAtDesc(userId);
         return ResponseEntity.ok(questions);
     }
 
-    // 자신이 적은 모든 답변 조회
+    // 내 답변 조회(최신순 5개)
     @GetMapping("/my-answers")
-    public ResponseEntity<List<AnswerDto>> getMyAllAnswers(@RequestHeader("Authorization") String token){
+    public ResponseEntity<List<QuestionWithAnswersDto>> getMyAllAnswers(@RequestHeader("Authorization") String token){
         Long userId = jwtTokenProvider.getUserIdFromToken(token.substring(7));
-        User findUser = userService.getUserById(userId);
-        List<AnswerDto> answers = findUser.getAnswers().stream()
-                .map(this::convertToAnswerDto)
-                .collect(Collectors.toList());
+        List<QuestionWithAnswersDto> answers = answerService.getAnswersByUserIdByCreatedAtDesc(userId);
         return ResponseEntity.ok(answers);
     }
 
--- src/main/java/com/kaidey/yakchatproject/domain/scrap/repository/ScrapRepository.java
+++ src/main/java/com/kaidey/yakchatproject/domain/scrap/repository/ScrapRepository.java
@@ -15,6 +15,6 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
     @Query("SELECT s FROM Scrap s WHERE s.scraperId = :userId AND s.question.id = :questionId")
     List<Scrap> findByUserIdAndQuestionId(Long userId, Long questionId);
 
-    @Query("SELECT s FROM Scrap s JOIN FETCH s.question WHERE s.scraperId = :userId ORDER BY s.question.createdAt DESC LIMIT 5")
+    @Query("SELECT s FROM Scrap s JOIN FETCH s.question WHERE s.scraperId = :userId ORDER BY s.question.createdAt")
     List<Scrap> findByUserIdCreatedAtDesc(Long userId);
 }
