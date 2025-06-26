f574bad22b4a05407a823a43ef0567baa2dc0e82
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2025-06-26 23:35:32 +0900

    배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,14 +6,15 @@ ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 # 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f .jar)
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
   echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
+  kill -9 $CURRENT_PID
+  sleep 3
 fi
 
 TIME_NOW=$(date +%c)
@@ -24,7 +25,7 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-source ~/.bashrc
+source /home/ubuntu/.bashrc
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
