68138d8ae50c002e152099c38dad7061dc2d6275
pparkcoder <pparkcoder@gmail.com>
2025-09-13 22:57:39 +0900

    fix : 답변 조회 채택된 순, 생성 순 으로 변경

--- src/main/java/com/kaidey/yakchatproject/domain/answer/repository/AnswerRepository.java
+++ src/main/java/com/kaidey/yakchatproject/domain/answer/repository/AnswerRepository.java
@@ -1,7 +1,6 @@
 package com.kaidey.yakchatproject.domain.answer.repository;
 
 import com.kaidey.yakchatproject.domain.answer.entity.Answer;
-import com.kaidey.yakchatproject.domain.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,14 +10,24 @@ import java.util.List;
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
     List<Answer> findByQuestionIdOrderByCreatedAtDesc(Long questionId);
+
     List<Answer> findByQuestionIdAndUserIdOrderByCreatedAtDesc(Long questionId, Long userId);
+
     boolean existsByQuestionIdAndIsAcceptedTrue(Long questionId);
+
     List<Answer> findByQuestionIdOrderByIsAcceptedDescCreatedAtAsc(Long questionId);
-    List<Answer> findTop5ByUserIdOrderByCreatedAtDesc(Long userId);
+
+    List<Answer> findTop5ByUserIdOrderByIsAcceptedDescCreatedAtDesc(Long userId);
 
     @Query("SELECT a FROM Answer a JOIN FETCH a.question WHERE a.question.id IN :questionIds")
     List<Answer> findByQuestionIdInOrderByCreatedAtDesc(List<Long> questionIds);
 
     @Query("SELECT a FROM Answer a JOIN FETCH a.question WHERE a.question.id IN :questionIds AND a.isAccepted = :isAccepted")
     List<Answer> findByQuestionIdAndIsAcceptedInOrderByCreatedAtDesc(List<Long> questionIds, Boolean isAccepted);
+
+    // List<Answer> findAllByIsAcceptedTrueOrderByIsAcceptedDescCreatedAtDesc(); // ✅ 올바른 형식
+
+    List<Answer> findAllByOrderByIsAcceptedDescCreatedAtDesc();
+
+
 }
